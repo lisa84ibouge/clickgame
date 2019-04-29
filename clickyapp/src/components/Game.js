324e7b87fbb793c602bf21bb36bffb1ecb756098
Lisa Leslie <lisar@ibouge.com>
2019-04-28 23:23:24 -0700

    updated MVC structure, image assets of memory game

---
+++ clickyapp/src/components/Game.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import images from "../images.js"
+
+class Game extends React.component {
+    state = {
+    allImages: images,
+    displayImages: images.slice(0,4),
+    highScore: 0, 
+    imagesSelected: []
+    }
+    render() {
+        return (
+            <div className="Game">
+            <h1>Clicky Game</h1>
+           </div>
+    )
+    }
+  }
+  
+  export default Game;
---
+++ clickyapp/src/images.js
@@ -0,0 +1,44 @@
+[
+    {
+      "id": 1,
+      "name": "Afghanistan",
+      "image": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Afghanistan.png"
+    },
+    {
+      "id": 2,
+      "name": "Albania",
+      "image": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Albania.png"
+    },
+    {
+      "id": 3,
+      "name": "Algeria",
+      "image": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Algeria.png"
+    },
+    {
+      "id": 4,
+      "name": "Andorra",
+      "image": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Andorra.png"
+    },
+    {
+      "id": 5,
+      "name": "Angola",
+      "image": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Angola.png"
+    },
+    {
+      "id": 6,
+      "name": "Antigua and Barbuda",
+      "image": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Antigua.png"
+    },
+    {
+      "id": 7,
+      "name": "Argentina",
+      "image": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Argentina.png"
+    },
+    {
+      "id": 8,
+      "name": "Armenia",
+      "image": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Armenia.png"
+    }
+   ]
+   
+   
