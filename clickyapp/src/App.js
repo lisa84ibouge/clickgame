58b19f56b4158b99b58725dcd95bda53599ed384
Lisa Leslie <lisar@ibouge.com>
2019-04-30 17:49:31 -0700

    updated react files / components

--- clickyapp/src/App.js
+++ clickyapp/src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Game from "./components/Game";
+import Readme from './readme'
+import Card from './components/card'
+import Board from './components/board'
+import initializeDeck from "./deck"
 
+export default function (App) {
+  const [cards, setCards] = useState([])
+  const [ flipped, setFlipped] = useState([])
 
-function App() {
+  useEffect(() => {
+    setCards(initializeDeck())
+  }, [])
+
+  const handleClick = (id) => 
+    setFlipped([ ...flipped, id])
+
+  
+  
   return (
-    <div className="App">
-     <Game/>
+    <div>
+    <h2>Can you remember where the cards are?</h2>
+    <Board
+    cards= {cards}
+    flipped = {flipped}
+    handleClick = {handleClick} />
     </div>
-  );
+  )
 }
-
-export default App;
