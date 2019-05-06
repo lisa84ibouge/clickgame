48a0f8117dc7f789f38a6079f869e90dc0fcab14
Lisa Leslie <lisar@ibouge.com>
2019-05-06 15:51:11 -0700

    last code addition

--- clickyapp/src/App.jsx
+++ clickyapp/src/App.jsx
@@ -1,4 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, {
+  useState,
+  useEffect
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Game from "./components/Game";
@@ -8,12 +11,12 @@ import Board from './components/board'
 //import images from '.images/'
 
 import initializeDeck from "./deck"
-import { isArgumentPlaceholder } from '@babel/types';
+
 
 export default function (App) {
   const [cards, setCards] = useState([])
-  const [ flipped, setFlipped] = useState([])
-  const [ dimension, setDimension] = useState(400)
+  const [flipped, setFlipped] = useState([])
+  const [dimension, setDimension] = useState(400)
   const [solved, setSolved] = useState([])
   const [disabled, setDisabled] = useState(false)
 
@@ -23,28 +26,44 @@ export default function (App) {
   }, [])
 
   useEffect(() => {
+    preloadImages()
+  }, cards)
+
+  useEffect(() => {
     const resizeListener = window.addEventListener('resize', resizeBoard)
     return () => window.removeEventListener('resize', resizeListener)
   })
 
   const handleClick = (id) => {
     setDisabled(true)
-    if (flipped.length === 0){
+    if (flipped.length === 0) {
       setFlipped([id])
       setDisabled(false)
     } else {
-      if(sameCardClicked( id))
-      return 
+      if (sameCardClicked(id))
+        return
       setFlipped([flipped[0], id])
       if (isMatch(id)) {
-        setSolved([... solved, flipped[0], id])
+        setSolved([...solved, flipped[0], id])
         resetCards()
       } else {
         setTimeout(resetCards, 2000)
       }
     }
-   
+
   }
+
+  const preloadImages = () => {
+    cards.map((card) => {
+
+      const src = `/img/${card.type}.png`
+      console.log(src)
+      new Image().src = src
+    })
+    
+
+  }
+
 
   const resetCards = () => {
     setFlipped([])
@@ -52,7 +71,7 @@ export default function (App) {
   }
 
   const sameCardClicked = (id) => flipped.includes(id)
-  
+
   const isMatch = (id) => {
     const clickedCard = cards.find((card) => card.id === id)
     const flippedCard = cards.find((card) => flipped[0] === card.id)
@@ -63,19 +82,27 @@ export default function (App) {
     setDimension(Math.min(
       document.documentElement.clientWidth,
       document.documentElement.clientHeight,
-      ),
-      )
+    ), )
   }
-  return (
-    <div>
-    <h2>Can you remember where the cards are?</h2>
-    <Board dimension={dimension}
-    cards= {cards}
-    flipped = {flipped}
-    handleClick = {handleClick} 
-    disabled={disabled}
-    solved={solved}
-    />
-    </div>
+  return ( <div>
+    <h2>Can you remember where the cards are?</h2> <Board dimension = {
+      dimension
+    }
+    cards = {
+      cards
+    }
+    flipped = {
+      flipped
+    }
+    handleClick = {
+      handleClick
+    }
+    disabled = {
+      disabled
+    }
+    solved = {
+      solved
+    }
+    /> </div>
   )
 }
