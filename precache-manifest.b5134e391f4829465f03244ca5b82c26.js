5c46c82491401c7c1021f8a253ee9564e1cb424b
Lisa Leslie <lisar@ibouge.com>
2019-05-07 19:55:45 -0700

    Updates

---
+++ precache-manifest.b5134e391f4829465f03244ca5b82c26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbba3373e584f11fee50c7b81bdd2c04",
+    "url": "/clickgame/index.html"
+  },
+  {
+    "revision": "52585719d52df5f1a861",
+    "url": "/clickgame/static/css/main.d519b5d5.chunk.css"
+  },
+  {
+    "revision": "10bf244ca692441702db",
+    "url": "/clickgame/static/js/2.1b3a0643.chunk.js"
+  },
+  {
+    "revision": "52585719d52df5f1a861",
+    "url": "/clickgame/static/js/main.187a5df4.chunk.js"
+  },
+  {
+    "revision": "550c79de082ae6f7d9ff",
+    "url": "/clickgame/static/js/runtime~main.5bc58040.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickgame/static/media/logo.5d5d9eef.svg"
+  }
+]);
